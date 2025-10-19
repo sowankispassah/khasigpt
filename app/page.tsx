06e18d85564e6f44171bbb0c4bae0c6e1fb09c76
sowankispassah <sowankispassah@users.noreply.github.com>
2025-10-19 15:35:57 +0530

    Simplify root page export

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,2 @@
-import type { ReactNode } from "react";
-import ChatLayout from "./(chat)/layout";
-import ChatPage from "./(chat)/page";
-
-export default async function RootPage() {
-  const page = await ChatPage();
-  return ChatLayout({ children: page as ReactNode });
-}
+export { default } from "./(chat)/page";
 
