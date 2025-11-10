ed5c31fb6394d489ef896d64e5cfce3e9ac45ff8
sowankispassah <sowankispassah@users.noreply.github.com>
2025-11-11 02:35:44 +0530

    fix deploy coupon

--- lib/errors.ts
+++ lib/errors.ts
@@ -111,9 +111,6 @@ export function getMessageByErrorCode(errorCode: ErrorCode): string {
       return "The request couldn't be processed. Please check your input and try again.";
     case "bad_request:coupon":
       return "Coupon is invalid or expired.";
-    case "bad_request:api:coupon_invalid":
-      return "Coupon is invalid or expired.";
-
     case "bad_request:activate_gateway":
       return "AI Gateway requires a valid credit card on file to service requests. Please visit https://vercel.com/d?to=%2F%5Bteam%5D%2F%7E%2Fai%3Fmodal%3Dadd-credit-card to add a card and unlock your free credits.";
 
