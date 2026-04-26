902c278ebeb8dd4cdfaf1b7e63a6552971f1d7c9
sowankispassah <sowankispassah@users.noreply.github.com>
2026-04-26 19:04:17 +0530

    Remove native app from web repo tracking

--- native/src/utils/id.ts
+++
@@ -1,8 +0,0 @@
-export function generateUUID() {
-  const template = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx";
-  return template.replace(/[xy]/g, (char) => {
-    const random = Math.floor(Math.random() * 16);
-    const value = char === "x" ? random : (random & 0x3) | 0x8;
-    return value.toString(16);
-  });
-}
